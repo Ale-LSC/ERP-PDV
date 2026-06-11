e9e0412db9fefc776ca3bf604dc1f108ba45a6e3
Ale-LSC <aleh.caputt@gmail.com>
2026-06-11 01:02:53 -0300

    feat: setup backend, frontend, postgres and drizzle~

---
+++ api/src/database/schema/users.schema.ts
@@ -0,0 +1,14 @@
+import { pgTable, uuid, varchar, timestamp, boolean } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+
+  name: varchar('name', { length: 120 }).notNull(),
+  email: varchar('email', { length: 180 }).notNull().unique(),
+  passwordHash: varchar('password_hash', { length: 255 }).notNull(),
+
+  isActive: boolean('is_active').default(true).notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
